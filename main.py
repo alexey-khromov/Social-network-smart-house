8c97fddae118bedf2628b3ea7da8bc099973799a
alexey-khromov <alexey-khromov@users.noreply.github.com>
2018-09-21 17:46:34 +0300

    Changed files for recognition infrastructure

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 from flask import Flask, jsonify, render_template, request
 from bs4 import BeautifulSoup, Comment
-from urllib.request import urlopen
 from fbrecog import FBRecog
 import requests
 import os
 
-'''
-path = 'mypic.jpg'
-access_token = 'EAAYeBD26ZAQkBAPZBBURrrKoaXaIGOsTVLGHl9cUh0NYCo8PkCl0WIvZCgE9V2zUESxlpsaVpTgZC7sjJuc9ltDZAFtRrxjrT0bj7u9MQGfqRFjzfAIGVSCZAOfGbLzinqZADjauLZCmUYZBYNOmZCWVZAmbRKVR5zFRj8ZCnfAvIDLO4GKKYVFS5mNXVVpcieSkbv4rjEcKqRoMOcdqkZCuyWDnZBrlTt9XmrYs4ZD'
-cookies = 'sb=xDdxW6gnARIAtC4XMxe0BTwL; datr=xDdxW7Ne7c2Tg4zreHGGsN8H; locale=en_US; dpr=1.5; pl=n; spin=r.4202043_b.trunk_t.1534169873_s.1_v.2_; act=1534169897844%2F3; c_user=100027703129886; xs=3%3ADZ81QAh8MPsC6g%3A2%3A1534169906%3A13822%3A-1; fr=0TeCrTm3A0MBtwlTt.AWWu3NCJcvYikAivBG9oC_YCZPs.BbcTfE.Qp.Ftx.0.0.BbcZMy.; presence=EDvF3EtimeF1534169900EuserFA21B27703129886A2EstateFDutF1534169900866CEchFDp_5f1B27703129886F2CC; wd=1113x830'
-fb_dtsg = 'AQH94-Qo091F:AQEa7lhShlER'
-
-# Instantiate the recog class
-recog = FBRecog(access_token, cookies, fb_dtsg)
-
-# Recog class can be used multiple times with different paths
-# mike = 123059741964232
-#temp = recog.get_user_data('/me/friends?field')
-
-#recognized = recog.recognize(path)
-#friend_name = recognized[0]['name']
-'''
+#-------------------------------------------------------------------------------
+# Environment Variables
+#-------------------------------------------------------------------------------
 
 try:
     USERNAME = os.environ['USER_MAIL_PROJ']
@@ -29,66 +15,114 @@ except KeyError:
    print("Please set the environment variables: USER_MAIL_PROJ, PASSWORD_PROJ")
    exit(1)
 
-app = Flask(__name__, template_folder='./')
-PROTECTED_URL = 'https://www.facebook.com/asia.zhivov/likes?lst=1198688678%3A843054236%3A1535896155'
-i = -1
-global sources
 
-@app.route('/') # <string:page_name>/
+#-------------------------------------------------------------------------------
+# Global Variables
+#-------------------------------------------------------------------------------
+
+app = Flask(__name__, template_folder='./')
+global likes_list_index
+global sources
+global curr_friend_name
+
+#-------------------------------------------------------------------------------
+# Flask URLs
+#-------------------------------------------------------------------------------
+
+@app.route('/')
 def render_static():
     return render_template('show_main.html')
 
+#-------------------------------------------------------------------------------
+
 @app.route("/getNextFrame", methods=['GET'])
-def getData():
-    global i
-    global sources
+def getNextFrame():
+    return _get_liked_page(likes_list_index+1)
 
-    next_page_name = list(sources.keys())[i]
-    friend_name = 'NAME_OF_MIKE'
-    res = jsonify({'next_url': '',
-                   'person_name': '',
-                   'page_name': '',
-                   'status': 'recognizing'
-                   })
-    if i > -1:
-        res = jsonify({'next_url': sources[next_page_name],
-                       'person_name': friend_name,
-                       'page_name': next_page_name,
-                       'status': 'recognized'
-                       })
+#-------------------------------------------------------------------------------
 
-    i = (i + 1) % len(sources)
+@app.route("/getPrevFrame", methods=['GET'])
+def getNextFrame():
+    return _get_liked_page(likes_list_index-1)
+
+#-------------------------------------------------------------------------------
+
+@app.route("/getPersonName", methods=['GET','POST'])
+def getPersonName():
+
+    global curr_friend_name
+    path = request.args.get('image_src') # image link passed from javascript
+    access_token = 'EAAYeBD26ZAQkBAGZARk0n7FUJHr9ogTmdkEjprDgA7ZAIN2mb3KGM4JP4SDRujYIRz38OEqxJMB8HMzZCmiKt2pXS1ZCNK0xZCCnfwF6CJyq4YTgz2VlL84yL9ZBj5FameBL6d5uGGWCHPc8bjYC4mVOn0EMfZAuHZBEuh1q7faxyt8mfb2lROcYBRKEtU2Fvvn3VyMexAVZASOiigDgW9pAkQYFpXz59jxZCsZD'
+    cookies = 'datr=vyE3WK59AKHob24eJFofccbS; sb=1CE3WLBBfN52DzpYJW81Fl-E; ; dpr=1.25; locale=en_US; pl=n; spin=r.4337968_b.trunk_t.1537532329_s.1_v.2_; act=1537532373473%2F5; c_user=100027703129886; xs=16%3At3Ha5ucHq6yxLg%3A2%3A1537532374%3A13822%3A-1; fr=0BF4Qlml0lg3T6JpB.AWV3-y7bMhKTugzLs8JzD24V8NQ.Ba-_AL.PF.Fuk.0.0.BbpOHW.; presence=EDvF3EtimeF1537532378EuserFA21B27703129886A2EstateFDutF1537532378401CEchFDp_5f1B27703129886F2CC; wd=1110x1038'
+    fb_dtsg = 'AQHufDWSC85X:AQEW6RNDXeCH'
+
+    fb_recog_obj = FBRecog(access_token, cookies, fb_dtsg)
+    recognized_Pfriends_list = fb_recog_obj.recognize(path)
+    if (len(recognized_Pfriends_list) > 0):
+        new_friend_name = recognized_Pfriends_list[0]['name']
+        if (curr_friend_name) and (new_friend_name != curr_friend_name):
+            curr_friend_name = new_friend_name
+            _create_likes_list()
+            likes_list_index = 0
+        res = {'person_name': curr_friend_name,
+               'status': 'recognized'}
+    else:
+        res = jsonify({'person_name': '',
+                       'status': 'recognizing'})
+
     return res
 
 
+#-------------------------------------------------------------------------------
+# Help Functions
+#-------------------------------------------------------------------------------
 
-def login(session, email, password):
+def _login(session, email, password):
     '''
     Attempt to login to Facebook. Returns cookies given to a user
     after they successfully log in.
     '''
 
     # Attempt to login to Facebook
-    response = session.post('https://m.facebook.com/login.php', data={ 'email': email, 'pass': password}, allow_redirects=False)
-
+    response = session.post('https://m.facebook.com/login.php',
+                            data={'email': email, 'pass': password},
+                            allow_redirects=False)
     assert response.status_code == 302
     assert 'c_user' in response.cookies
     return response.cookies
 
+#-------------------------------------------------------------------------------
 
-if __name__ == '__main__':
+def _get_liked_page(next_index):
+    global likes_list_index
+    global sources
+    global curr_friend_name
+
+    like_index = next_index % len(sources)
+    next_page_name = list(sources.keys())[likes_list_index]
+    if likes_list_index > -1:
+        res = jsonify({'next_url': sources[next_page_name],
+                       'page_name': next_page_name,
+                       'status': 'new_fb_page'})
+    return res
+
+#-------------------------------------------------------------------------------
+
+def _create_likes_list():
+    global curr_friend_name
+    global sources
 
     session = requests.session()
-    cookies = login(session, USERNAME, PASSWORD)
-
-    response = session.get(PROTECTED_URL, cookies=cookies, allow_redirects=False)
+    cookies = _login(session, USERNAME, PASSWORD)
+    splitted_name_of_curr_friend = 'Asia Zhivov'.lower().split(' ') # TODO: curr_friend_name
+    dotted_name_of_curr_friend = splitted_name_of_curr_friend[0] + '.' + splitted_name_of_curr_friend[-1]
+    likes_url = 'https://www.facebook.com/' + dotted_name_of_curr_friend +'/likes?lst=1198688678%3A843054236%3A1535896155'
+    response = session.get(likes_url, cookies=cookies, allow_redirects=False)
     assert response.text.find('Home') != -1
 
-    # to visually see if you got into the protected page, I recomend copying
-    # the value of response.text, pasting it in the HTML input field of
-    # http://codebeautify.org/htmlviewer/ and hitting the run button
+    # For visual see, use: http://codebeautify.org/htmlviewer/
     soup = BeautifulSoup(response.content, "html.parser")
-    comments = soup.findAll(text=lambda text: isinstance(text, Comment))
+    comments = soup.findAll(text=lambda text: isinstance(text, Comment))    # the liked pages are in comment
     final_likes_dict = {}
     for comment in comments:
         soup_inner = BeautifulSoup(comment, 'html.parser')
@@ -97,35 +131,14 @@ if __name__ == '__main__':
             div_like_link = div_like.find('a')
             final_likes_dict[div_like_link.text] = div_like_link['href']
 
-    #for liked_page, link_page in final_likes_dict.items():
-    #    print(link_page)
-
-    global sources
     sources = final_likes_dict
 
-    # For posts retrieving from pages
-    '''
-    posts_links_to_show = []
-    for liked_page, link_page in final_likes_dict.items():
-        #print(link_page)
-        page_name = link_page.split('/')[-2]
-        new_link = link_page.replace(page_name, 'pg/' + page_name + '/posts/?ref=page_internal')
-        #print(new_link)
-        posts_links_to_show.append(new_link)
 
-    final_posts_list = []
-    for liked_page in posts_links_to_show:
-        response = urlopen(liked_page)
-        soup = BeautifulSoup(response.read(), "html.parser")
-        posts_object = soup.find('span', {'class': 'fsm fwn fcg'})
-        if posts_object is not None:
-            div_like_link = posts_object.find('a')
-            link_to_append = 'https://www.facebook.com' + div_like_link['href']
-            final_posts_list.append(link_to_append)
+#-------------------------------------------------------------------------------
+# Main Function
+#-------------------------------------------------------------------------------
 
-    for post in final_posts_list:
-        print(post)
-    '''
-
+if __name__ == '__main__':
     app.run(debug=True)
+
 
